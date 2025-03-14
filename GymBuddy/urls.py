12ca99da5c4a153e2ba7fae1e707a24b8479022b
adhalail <2311102441005@umkt.ac.id>
2025-03-15 00:27:19 +0800

    third attempt on django - created apps - adjusted apps - makemigrations and migrate app to database table - adjusted django-admin table/models - uploaded images

--- GymBuddy/urls.py
+++ GymBuddy/urls.py
@@ -17,9 +17,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+######################## ini untuk menampilkan gambar yang sudah diupload pada folder media ############################
+from django.conf import settings
+from django.conf.urls.static import static
+
 from GymBuddy.views import home, about
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("",  home, name="home"),
     path("about",  about, name="about"),
 ]
+
+
+######################## ini untuk menampilkan gambar yang sudah diupload pada folder media ############################
+if settings.DEBUG: 
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
